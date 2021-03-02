6856130c3b0985e02aea84fcff001f735b8806be
Zhenbo Li <3221521+Endle@users.noreply.github.com>
2021-03-02 14:45:10 +0800

    Remove vkd3d

--- compile_wine.sh
+++ compile_wine.sh
@@ -26,10 +26,69 @@ export FRAMEWORK_SEARCH_PATHS="/opt/local/Library/Frameworks"
 
 cd $WINE_BUILD
 $WINE_SRC/configure         --enable-win64 \
-                            --with-vkd3d \
-                            --with-vulkan \
-                            ac_cv_lib_soname_MoltenVK=libMoltenVK.dylib \
-                            ac_cv_lib_soname_vulkan=
+                            --disable-option-checking \
+--without-alsa \
+--without-capi \
+--with-cms \
+--with-coreaudio \
+--with-cups \
+--with-curses \
+--without-dbus \
+--with-faudio \
+--without-fontconfig \
+--with-freetype \
+--with-gcrypt \
+--with-gettext \
+--without-gettextpo \
+--without-gphoto \
+--with-gnutls \
+--without-gsm \
+--without-gssapi \
+--with-gstreamer \
+--without-hal \
+--without-inotify \
+--with-jpeg \
+--with-jxrlib \
+--without-krb5 \
+--with-ldap \
+--with-mingw \
+--with-mpg123 \
+--without-netapi \
+--with-openal \
+--with-opencl \
+--with-opengl  \
+--without-oss \
+--with-pcap \
+--with-png \
+--with-pthread \
+--without-pulse \
+--without-quicktime \
+--without-sane \
+--with-sdl \
+--with-tiff \
+--without-udev \
+--with-unwind \
+--with-usb \
+--without-v4l2 \
+--without-vkd3d \
+--without-xattr \
+--with-xml \
+--with-xslt \
+--with-osmesa \
+--with-xcomposite \
+--with-xcursor \
+--with-xfixes \
+--with-xinerama \
+--with-xinput \
+--with-xinput2 \
+--with-xrandr \
+--with-xrender \
+--with-xshape \
+--with-xshm \
+--with-xxf86vm \
+--with-x \
+--x-include=/opt/X11/include \
+--x-lib=/opt/X11/lib
 
 make -j$(sysctl -n hw.ncpu 2>/dev/null)
 make install-lib DESTDIR="$PREFIXFOLDER"
